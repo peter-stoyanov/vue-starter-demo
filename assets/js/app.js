96e5afc0adfc305e1bd42e34675d38617ff28c0f
Nina Stoyanova <nina.stoqnova91@gmail.com>
2021-04-24 16:41:58 +0300

    why us section component

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 import * as ButtonCounter from "./components/ButtonCounter.js";
 import * as CustomHeader from "./components/CustomHeader.js";
 import * as HeroSection from "./components/HeroSection.js";
-
+import * as WhyUsSection from "./components/WhyUsSection.js"
 
 var app = new Vue({
     el: '#app',
